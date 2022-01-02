29a0ef013a9617b4a237879c8785b033d1f673fe
Jens Horstmann <jens@firecrown.pro>
2022-01-02 19:19:12 +0100

    init project

---
+++ bin/widgetbook_hosting.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  final env = Platform.environment;
+  final branch = env['GITHUB_REF_NAME'];
+  final actionVersion = env['GITHUB_ACTION_REF'];
+
+  /// the user e.g. JensHor
+  final actor = env['GITHUB_ACTOR'];
+  final commitSha = env['GITHUB_SHA'];
+  final repository = env['GITHUB_REPOSITORY'];
+  final latestCommit = env['INPUT_LATESTCOMMIT'];
+  final runnerOS = env['RUNNER_OS'];
+
+  print('Branch: $branch');
+  print('Commit: $commitSha');
+  print('User: $actor');
+
+  print('Version: $actionVersion');
+  print('Runner OS: $runnerOS');
+
+  print('Arguments');
+  print(arguments);
+}
